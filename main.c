79dcca0ee2b043dd8241eecd592d8fbba7427904
Vladislav Prekel <misterptits@yandex.ru>
2020-06-24 16:02:01 +0700

    main.c

--- main.c
+++ main.c
@@ -36,32 +36,38 @@ int main(int argc, char** argv)
 
     if (a == -2)
     {
-        bool success = true;
         int res = 0;
-        printf("Введите IP сервера базы данных [localhost]: "); fflush(stdout);
+        printf("Введите IP сервера базы данных [localhost]: ");
+        fflush(stdout);
         res = InputLine(address, STRING_SIZE);
         if (res == 0) strcpy(address, "localhost");
-        success &= res >= 0;
-        printf("Введите порт сервера базы данных [5432]: "); fflush(stdout);
+        bool success = res >= 0;
+        printf("Введите порт сервера базы данных [5432]: ");
+        fflush(stdout);
         res = InputLine(port, STRING_SIZE);
         if (res == 0) strcpy(port, "5432");
-        success &= res >= 0;
-        printf("Введите название базы данных [dbmessenger]: "); fflush(stdout);
+        success = success && res >= 0;
+        printf("Введите название базы данных [dbmessenger]: ");
+        fflush(stdout);
         res = InputLine(dbname, STRING_SIZE);
         if (res == 0) strcpy(dbname, "dbmessenger");
-        success &= res >= 0;
-        printf("Введите пользователя базы данных [postgres]: "); fflush(stdout);
+        success = success && res >= 0;
+        printf("Введите пользователя базы данных [postgres]: ");
+        fflush(stdout);
         res = InputLine(dbUser, STRING_SIZE);
         if (res == 0) strcpy(dbUser, "postgres");
-        success &= res >= 0;
-        printf("Введите пароль пользователя базы данных [qwerty123]: "); fflush(stdout);
+        success = success && res >= 0;
+        printf("Введите пароль пользователя базы данных [qwerty123]: ");
+        fflush(stdout);
         res = InputLine(dbPassword, STRING_SIZE);
         if (res == 0) strcpy(dbPassword, "qwerty123");
-        success &= res >= 0;
+        success = success && res >= 0;
+
         if (!success)
         {
             return 10;
         }
+
         if (!Connect(address, port, dbname, dbUser, dbPassword))
         {
             return 11;
@@ -69,26 +75,51 @@ int main(int argc, char** argv)
 
         printf("Выберите действие:\n");
         printf("1. Войти\n");
-        printf("1. Зарегистрироваться\n");
-        printf("1. Создать таблицы\n");
-        printf("1. Удалить таблицы\n");
+        printf("2. Зарегистрироваться\n");
+        printf("3. Создать таблицы\n");
+        printf("4. Удалить таблицы\n");
 
         InputLine(num, STRING_SIZE);
-        int a = ParseInt(num, NULL);
+        int sucb = 0;
+        int b = ParseInt(num, &sucb);
+        if (sucb == 0)
+        {
+            return 13;
+        }
 
-        printf("Введите логин [User1]: "); fflush(stdout);
+        if (b == 3)
+        {
+            EnsureCreated();
+        }
+        if (b == 4)
+        {
+            Drop();
+        }
+
+        printf("Введите логин [User1]: ");
+        fflush(stdout);
         res = InputLine(user, STRING_SIZE);
         if (res == 0) strcpy(user, "User1");
         success &= res >= 0;
-        printf("Введите пароль [qwerty]: "); fflush(stdout);
+        printf("Введите пароль [qwerty]: ");
+        fflush(stdout);
         res = InputLine(password, STRING_SIZE);
         if (res == 0) strcpy(password, "qwerty");
         success &= res >= 0;
+
         if (!success)
         {
             return 12;
         }
 
+        if (b == 1)
+        {
+            Login(user, password);
+        }
+        if (b == 2)
+        {
+            Register(user, password);
+        }
     }
     if (a == -1)
     {
